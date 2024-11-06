6810702d533309fe6a93fbb2f61993856012c31b
丁俊杰 <10725046+JunJD@user.noreply.gitee.com>
2024-11-06 08:24:07 +0800

    chore: add @typescript-eslint plugins to devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,31 @@
 module.exports = {
-    extends: [
-      'next/core-web-vitals',
-      'plugin:@typescript-eslint/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': ['warn', { 
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_' 
-      }],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
-      '@typescript-eslint/ban-ts-comment': 'warn'
-    },
-    overrides: [
-      {
-        // 对于 JavaScript 文件禁用 TypeScript 相关规则
-        files: ['*.js'],
-        rules: {
-          '@typescript-eslint/no-unused-vars': 'off',
-          '@typescript-eslint/no-explicit-any': 'off'
-        }
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_' 
+    }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn'
+  },
+  overrides: [
+    {
+      // 对于 JavaScript 文件禁用 TypeScript 相关规则
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'off'
       }
-    ]
-  };
+    }
+  ]
+};
